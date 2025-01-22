d851f31193fd3795bcd18bc2a12c7aac2104de54
nada_azzam <nadaazzam123@outlook.com>
2025-01-22 03:58:02 +0200

    update spec files

--- src/app/directives/highlight/highlight.directive.spec.ts
+++ src/app/directives/highlight/highlight.directive.spec.ts
@@ -1,12 +1,8 @@
 import { HighlightDirective } from './highlight.directive';
-import { ElementRef } from '@angular/core';
 
 describe('HighlightDirective', () => {
-  // Create a stub for ElementRef if needed
-  const elementRefStub: ElementRef = { nativeElement: document.createElement('div') };
-
   it('should create an instance', () => {
-    const directive = new HighlightDirective(elementRefStub);
+    const directive = new HighlightDirective();
     expect(directive).toBeTruthy();
   });
 });
